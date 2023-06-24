ba988e32870f2e39b68125c8dc83e65edb5141ed
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-24 14:03:06 +0100

    initial commit

---
+++ src/return-total-sum-with-loop.ts
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param inputArray - array of numbers and strings
+ * @returns totalSum - the toatl of all numbers in inputArray
+ */
+
+function returnTotalSum(inputArray){
+    return 0;
+}
+
+export default inputArray;
