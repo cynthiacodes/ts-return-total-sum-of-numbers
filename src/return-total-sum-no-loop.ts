c96b7123576a4b75760ddc48d5fb9a87d754d53e
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-24 14:31:32 +0100

    initial commit for function and test without loop

---
+++ src/return-total-sum-no-loop.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param inputArray - array of numbers and strings
+ * @returns totalSum - the total of all numbers in inputArray
+ */
+
+function returnTotalSum (inputArray : (string | number )[]){
+
+
+    return 0;
+}
+
+export default returnTotalSum;
