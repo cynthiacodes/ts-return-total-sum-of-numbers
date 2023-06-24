06f556936a8f1b663d8c19b148135b41ec188664
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-24 14:25:21 +0100

    add function implementation and corrected export statement

--- src/return-total-sum-with-loop.ts
+++ src/return-total-sum-with-loop.ts
@@ -1,11 +1,19 @@
 /**
  * 
  * @param inputArray - array of numbers and strings
- * @returns totalSum - the toatl of all numbers in inputArray
+ * @returns totalSum - the total of all numbers in inputArray
  */
 
-function returnTotalSum(inputArray){
+function returnTotalSum (inputArray : (string | number )[]){
+    let totalSum = 0;
+    for(let element of inputArray){
+        if (typeof element === "number"){
+            totalSum += element;
+        }
+    return totalSum;
+    }
+
     return 0;
 }
 
-export default inputArray;
+export default returnTotalSum;
